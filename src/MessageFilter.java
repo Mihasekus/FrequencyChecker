efc084eeb57cea2ab9c214f1f7180205afd440b0
Peshko Mihail <mihalilusoid@gmail.com>
2016-10-07 15:32:59 +0300

    MessageFilter is thread-safe now.

--- src/MessageFilter.java
+++ src/MessageFilter.java
@@ -1,28 +1,33 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class MessageFilter implements FrequencyChecker {
     private int maxMessagesAmount;
-    private ArrayList<Long> messageTimestamps;
+    private LinkedList<Long> messageTimestamps;
+
     public MessageFilter(int N) {
         if (N < 0) {
             throw new IllegalArgumentException("Message amount can`t be negative");
         }
         maxMessagesAmount = N;
-        messageTimestamps = new ArrayList<Long>(N);
+        messageTimestamps = new LinkedList<Long>();
     }
 
     public boolean isAllowed() {
-        while (messageTimestamps.size() > 0) {
-            if (System.currentTimeMillis() - messageTimestamps.get(0) > 60000) {
-                messageTimestamps.remove(0);
-            } else {
-                break;
+        synchronized (this) {
+            while (messageTimestamps.size() > 0) {
+                if (System.currentTimeMillis() - messageTimestamps.get(0) > 60000) {
+                    messageTimestamps.remove(0);
+                } else {
+                    break;
+                }
             }
+
+            if (messageTimestamps.size() < maxMessagesAmount) {
+                messageTimestamps.add(System.currentTimeMillis());
+                return true;
+            }
+
+            return false;
         }
-        if (messageTimestamps.size() < maxMessagesAmount) {
-            messageTimestamps.add(System.currentTimeMillis());
-            return true;
-        }
-        return false;
     }
 }
